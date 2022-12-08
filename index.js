0aecb9e64a12952481088e68ea6fca48144eaa6a
Ernesto De la Rosa <ernestodelazamora@gmail.com>
2022-12-08 14:44:48 -0600

    archivos agregados

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+router.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/templates/index.html'));
+});
+
+router.get('/contacto', (req, res) => {
+	res.sendFile(path.join(__dirname + '/templates/contacto.html'));
+});
+
+router.get('/perfil', (req, res) => {
+	res.sendFile(path.join(__dirname + '/templates/perfil.html'));
+});
+
+router.post('/', (req, res) => {
+	res.send(
+		'El usuario ' + req.body.first_name + ' ha sido registrado con éxito'
+	);
+});
+
+app.use('/', router);
+app.listen(process.env.port || 8080);
+
+console.log('Running at Port 8080');
